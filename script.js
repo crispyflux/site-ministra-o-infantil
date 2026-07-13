07012ff5f2de420c9cc3e8b99d7e710464bd2ba8
crispyDev <christofferarasant@gmail.com>
2026-07-12 22:18:04 -0300

    Add contact button functionality to script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btnMobile = document.querySelector('.btn-mobile')
 const navMobile = document.getElementById('container-mobile')
+const btncontatar = document.getElementById('btn-contatar')
 
 btnMobile.addEventListener('click', () => {
     navMobile.classList.toggle('active')
+})
+
+btncontatar.addEventListener('click', () => {
+    window.open("https://wa.me/5511942049146")
 })
